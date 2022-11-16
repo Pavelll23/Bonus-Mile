f7052c0623119d4a78c41ade057bd3e3092b53ce
Pavel Kozlov <cozlov.pavel@mail.ru>
2022-11-16 21:46:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int Ticket = 100; // Стоимость билета
+        int a = 20; // Количество рублей для одной бонусной мили
+        int mile = Ticket / a; // Количество бонусных милей
+
+
+        System.out.println("Бонусные мили:" + mile);
+    }
+
+}
